abcfd8b95f78a3ffec38c5503a6dde1f07e1bcc3
vinsnega <snegavin16@gmail.com>
2026-09-11 15:36:03 +0530

    Add files via upload

---
+++ Extra_Assignment_Question_-1_Vincy.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+print("HOPE AI")
+
+
+# In[24]:
+
+
+name=input("Enter your institute name? : ")
+
+
+# In[4]:
+
+
+name= input("Enter your name : ")
+age=int(input("Enter your age : "))
+School=input("Enter your school name : ")
+degree=input("Enter your degree : ")
+
+
+# In[6]:
+
+
+#Addition
+a=43
+b=34
+add=a+b
+print("a=",a)
+print("b=",b)
+print("add=",add)
+
+
+# In[9]:
+
+
+#subtraction
+a=43
+b=34
+Sub=a-b
+print("a=",a)
+print("b=",b)
+print("Sub =",Sub)
+
+
+# In[10]:
+
+
+#Multiplication
+a=63
+b=97
+Mul =a*b
+print("a=",a)
+print("b=",b)
+print("Mul = ",Mul)
+
+
+# In[16]:
+
+
+#Division
+a=76
+b=31
+Div=a/b
+print("a=",a)
+print("b=",b)
+print("Float Div = ",Div)
+
+
+# In[20]:
+
+
+#Floor division
+a=76
+b=31
+fdiv=a//b
+print("a=",a)
+print("b=",b)
+print("Floor Div =",fdiv)
+
+
+# In[21]:
+
+
+#Modulo
+a=76
+b=31
+mod=a%b
+print("a= ",a)
+print("b=",b)
+print("Modulo = ",mod)
+
+
+# In[22]:
+
+
+a=4
+b=4
+Pow=a**b
+print("a= ",a)
+print("b= ",b)
+print("Power =",Pow)
+
+
+# In[ ]:
+
+
+
+
